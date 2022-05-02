ffaea2c49515091b34afdf533787c105fc7b9aa9
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-05-01 19:17:40 -0700

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo {
     public static int intAddition(int num1, int num2) {
         int combinedInt = 0;
         combinedInt = num1 + num2;
-        return 0;
+        return combinedInt;
     }
 }
