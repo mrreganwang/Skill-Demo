d354860fa0b87f87b6be41745dc8e0a39cbe6c18
mrreganwang <103270847+mrreganwang@users.noreply.github.com>
2022-05-01 19:14:11 -0700

    Test added

--- Demo.java
+++ Demo.java
@@ -1,1 +1,7 @@
-
+public class Demo {
+    public static int intAddition(int num1, int num2) {
+        int combinedInt = 0;
+        combinedInt = num1 + num2;
+        return 0;
+    }
+}
---
+++ DemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class DemoTest {
+    @Test
+    public void testIntAddition() {
+        assertEquals(3, Demo.intAddition(1,2));
+    }
+}
